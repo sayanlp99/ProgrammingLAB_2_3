4ea9d51bd4847b36235d4a1c6fc9916baea90aa2
sayanlp99 <sayan.chakraborty1999@gmail.com>
2019-07-09 20:24:35 +0530

    update

--- DATA STRUCTURE/arrayMerge.c
+++ DATA STRUCTURE/arrayMerge.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    int a[50],b[50],c[100],n,m,i,j,s;
+    int a[50],b[50],c[100],n,m,i,j,s,t;
     printf("Enter range for First Array:");
     scanf("%d",&n);
     printf("Enter elements for First Array:\n");
@@ -20,6 +20,15 @@ int main(){
     s=m+n;
     for(i=0,j=n;i<n,j<s;i++,j++)
         c[j]=b[i];
+    for(i=0;i<n-1;i++){
+        for(j=i+1;j<n;j++){
+            if(c[i]>c[j]){
+                t=c[j];
+                c[j]=c[i];
+                c[i]=t;
+            }
+        }
+    }
     printf("Merged Array:\n");
     for(i=0;i<s;i++)
         printf("%d ",c[i]);
