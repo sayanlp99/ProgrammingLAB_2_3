6b7c4f64e350fee8e03554cc3db920c96b52bd55
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2019-08-05 14:10:47 +0530

    Create nptel2.cpp

--- NPTEL/nptel2.cpp
+++ NPTEL/nptel2.cpp
@@ -1,1 +1,23 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+	int i,j,n;
+	cin>>n;
+	for(i=0;i<n;i++){
+        for(j=0;j<3*n;j++)
+		    cout<<"*";
+        cout<<endl;
+	}
+	for(int x=0;x<2;x++){
+    for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			cout<<" ";
+        }
+        for(j=0;j<n;j++){
+			cout<<"*";
+		}
+        cout<<endl;
+	}
+}
+    return 0;
+}
