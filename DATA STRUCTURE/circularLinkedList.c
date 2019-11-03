c60c747b5f3c8e1e1445cbf9d679e58280b85b1a
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2019-11-03 09:54:01 +0530

    Update circularLinkedList.c

--- DATA STRUCTURE/circularLinkedList.c
+++ DATA STRUCTURE/circularLinkedList.c
@@ -72,3 +72,28 @@ void display(){
 		printf("Node value:%d  Node address=%u  Node next=%u\n",temp->info,temp,temp->next);
 	}
 }
+/*OUTPUT:-
+OPTIONS:-
+1. CREATE
+2. DELETE
+3. DISPLAY
+Enter Option:1
+Enter the node value :4
+Enter Option:1
+Enter the node value :7
+Enter Option:1
+Enter the node value :9
+Enter Option:1
+Enter the node value :2
+Enter Option:3
+Node value:4  Node address=11086160  Node next=11086176
+Node value:7  Node address=11086176  Node next=11086192
+Node value:9  Node address=11086192  Node next=11086208
+Node value:2  Node address=11086208  Node next=11086160
+Enter Option:2
+Deleted Node with value:4
+Enter Option:2
+Deleted Node with value:7
+Enter Option:3
+Node value:9  Node address=11086192  Node next=11086208
+Node value:2  Node address=11086208  Node next=11086192*/
