bfee0c085ac93874c166b74d72d57994ddf976de
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2019-11-09 19:41:09 +0530

    Update doublyLinkedList.c

--- DATA STRUCTURE/doublyLinkedList.c
+++ DATA STRUCTURE/doublyLinkedList.c
@@ -1,1 +1,51 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int info;
+    struct node *prev;
+    struct node *next;
+}*h, *temp, *temp1, *temp2, *temp3;
+void create();
+void insert();
+void delete();
+void count();
+void display();
+int count=0;
+int main(){
+    int s;
+    printf("OPTIONS:-\n1. CREATE\n2. INSERT\n3. DELETE\n4. COUNT\n5. DISPLAY\n");
+    while(1){
+        printf("Enter option:");
+        scanf("%d",&s);
+        switch(c){
+            case 1:
+                create();
+                break;
+            case 2:
+                insert();
+                break;
+            case 3:
+                delete();
+                break;
+            case 4:
+                count();
+                break;
+            case 5:
+                display();
+                break;
+            default:
+                printf("INVALID OPTION.\n");
+        }
+    }
+    return 0;
+}
+void create(){
+    int data;
+    temp =(struct node *)malloc(1*sizeof(struct node));
+    temp->prev = NULL;
+    temp->next = NULL;
+    printf("Enter value to node:");
+    scanf("%d", &data);
+    temp->info = data;
+    count++;
+}
