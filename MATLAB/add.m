618a9b18f3d776131974ce95570775c21d02bcfa
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2019-11-26 02:36:50 +0530

    Delete codes

--- MATLAB/add.m
+++
@@ -1,4 +0,0 @@
-a=input('Enter 1st number:');
-b=input('Enter 2nd number:');
-c=a+b;
-fprintf('Sum=%4u\n',c);
--- MATLAB/evenodd.m
+++
@@ -1,6 +0,0 @@
-m=input('Enter Number:');
-if mod(m,2) == 0
-    fprintf('%4u is Even\n',m);
-else
-    fprintf('%4u is Odd\n',m);
-end
