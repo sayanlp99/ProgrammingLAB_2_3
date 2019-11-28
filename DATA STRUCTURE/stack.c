95246ec9ab9b2b1ccedb962ee76c49f9d55dca8a
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2019-11-29 01:46:03 +0530

    Optimized stack.c

--- DATA STRUCTURE/stack.c
+++ DATA STRUCTURE/stack.c
@@ -2,56 +2,52 @@
 #define MAX 10
 int stack[MAX];
 int top=-1;
-void pop();
 void push();
+void pop();
 void display();
 int main(){
-	int select,item;
-	while(1){
-		printf("Choose option to perform:-\n1. PUSH\n2. POP\n3. DISPLAY\n:");
-		scanf("%d",&select);
-		switch(select){
-			case 1:
-				push();
-				break;
-			case 2:
-				pop();
-				break;
-			case 3:
-				display();
-				break;
-		}
-	}
-	return 0;
+    int ch;
+    printf("OPTIONS:\n1. PUSH\n2. POP\n3. DISPLAY\n");
+    while(1){
+        printf("ENTER OPTION:");
+        scanf("%d",&ch);
+        switch(ch){
+            case 1:
+                push();
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                display();
+                break;
+            default:
+                printf("INVALID OPTION.\n");
+        }
+    }
+    return 0;
 }
 void push(){
-	int item;
-	if(top==MAX-1)
-		printf("Stack is full\n");
-	else{
-		printf("Enter the Item:");
-		scanf("%d",&item);
-		top=top+1;
-		stack[top]=item;		
-	}
+    if(top==MAX-1)
+        printf("STACK OVERFLOW.\n");
+    else{
+        printf("ENTER ELEMENT:");
+        scanf("%d",&stack[++top]);
+    }
 }
 void pop(){
-	if(top==-1){
-		printf("Stack is Empty\n");
-	}
-	else{
-		printf("Deleted Item is:%d\n",stack[top]);
-		top=top-1;
-	}
+    if(top==-1)
+        printf("STACK UNDERFLOW.\n");
+    else
+        printf("DELETED VALUE:%d\n",stack[top--]);
 }
 void display(){
-	int i;
-	if(top==-1)
-		printf("Stack is Empty.\n");
-	else{
-		printf("The Stack is:-\n");
-		for(i=top;i>=0;--i)
-			printf("%d  ",stack[i]);
-		printf("\n");
-	}
+    if(top==-1)
+        printf("STACK EMPTY.\n");
+    else{
+        printf("ELEMENTS IN STACK:\n");
+        for(int i=top;i>=0;--i)
+            printf("%d  ",stack[i]);
+        printf("\n");
+    }
 }
