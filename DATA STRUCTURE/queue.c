c8c18dc304919d21f7bcc34cbb815440166136de
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2019-11-29 02:36:55 +0530

    Optimize queue.c

--- DATA STRUCTURE/queue.c
+++ DATA STRUCTURE/queue.c
@@ -1,65 +1,60 @@
 #include<stdio.h>
 #define MAX 10
-int front=-1,rear=-1,item;
 int queue[MAX];
+int front=-1,rear=-1;
 void insert();
 void delete();
 void display();
 int main(){
-	int select;
-	while(1){
-		printf("Enter Option:-\n1. INSERT\n2. DELETE\n3. DISPLAY\n:");
-		scanf("%d",&select);
-		switch(select){
-			case 1:
-					insert();
-					break;
-			case 2:
-					delete();
-					break;
-			case 3:
-					display();
-					break;
-			default:
-					printf("INVALID OPTION\n");
-		}
-	}
-	return 0;
+    int ch;
+    printf("ENTER OPTION:\n1. INSERT\n2. DELETE\n3. DISPLAY\n");
+    while (1){
+        printf("ENTER OPTION:");
+        scanf("%d",&ch);
+        switch(ch){
+            case 1:
+                insert();
+                break;
+            case 2:
+                delete();
+                break;
+            case 3:
+                display();
+                break;
+            default:
+                printf("INVALID OPTION.\n");
+        }
+    }
+    return 0;
 }
 void insert(){
-	if(rear==MAX-1)
-		printf("Queue Overflow\n");
-	else{
-		printf("Enter item:");
-		scanf("%d",&item);
-		rear=rear+1;
-		queue[rear]=item;
-		if(front==-1)
-			front=0;
-	}
+    if(rear==MAX-1)
+        printf("QUEUE OVERFLOW.\n");
+    else{
+        printf("ENTER ELEMENT:");
+        scanf("%d",&queue[++rear]);
+        if(front==-1)
+            front=0;
+    }
 }
 void delete(){
-	if (front==-1 || front>rear)
-		printf("Queue Underflow\n");
-	else{
-		item=queue[front];
-		printf("The deleted item:%d\n",item);
-		if(front==rear){
-			front=-1;
-			rear=-1;
-		}
-		else
-			front=front+1;
-	}
+    if(rear==-1 || front>rear)
+        printf("QUEUE UNDERFLOW.\n");
+    else{
+        printf("DELETED ITEM:%d\n",queue[front]);
+        if(front==rear)
+            front=rear=-1;
+        else
+            front=front+1;
+    }
 }
 void display(){
-	int i;
-	if(front==-1)
-		printf("Queue Underflow\n");
-	else{
-		printf("Elements in queue:- ");
-		for(i=front;i<=rear;i++)
-			printf("%d  ",queue[i]);
-		printf("\n");
-	}
+    if(rear==MAX-1)
+        printf("QUEUE EMPTY.\n");
+    else{
+        printf("ITEMS IN QUEUE:\n");
+        for(int i=front;i<=rear;i++)
+            printf("%d  ",queue[i]);
+        printf("\n");
+    }
 }
